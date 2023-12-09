6a08ff816cdea42b7d388cc85b38f8e48f5f66a6
Suresh Kumar <suresh189.kumar@gmail.com>
2023-12-09 21:22:25 +0530

    Add new file

---
+++ app/src/main/java/com/example/mycomposeapplication/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.mycomposeapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mycomposeapplication.ui.theme.MyComposeApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+
+    @Preview(showBackground = true, widthDp = 200, heightDp = 200)
+    @Composable
+    fun text(name:String = "Hello"){
+        Text(text = "$name Suresh Kumar")
+    }
+}
+
+
