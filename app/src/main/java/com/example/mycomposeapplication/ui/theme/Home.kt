de7ad1d13bfa23d626b54939a68db01bcf0eb140
Suresh Kumar <suresh189.kumar@gmail.com>
2023-12-09 21:52:57 +0530

    add in HomeKt

--- app/src/main/java/com/example/mycomposeapplication/ui/theme/Home.kt
+++ app/src/main/java/com/example/mycomposeapplication/ui/theme/Home.kt
@@ -4,6 +4,9 @@ package com.example.mycomposeapplication.ui.theme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 
 class Home : ComponentActivity() {
@@ -12,5 +15,11 @@ class Home : ComponentActivity() {
         setContent {
 
         }
+    }
+
+    @Preview(showBackground = true, widthDp = 200, heightDp = 200)
+    @Composable
+    fun text(name:String = "Hello"){
+        Text(text = "$name Suresh Kumar Feature1")
     }
 }
